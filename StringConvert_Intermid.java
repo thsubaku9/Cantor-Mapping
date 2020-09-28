f9744bedcc81fff8b5cb312777dbd23bb09c22e7
Kaustubh J <kaustubhkj@gmail.com>
2020-09-28 22:11:00 +0530

    Add files via upload

--- StringConvert_Intermid.java
+++ StringConvert_Intermid.java
@@ -1,5 +1,6 @@
 package FastSort;
 
+import java.util.ArrayList;
 
 class StringConvert_Intermid {
     static float x = 30;
@@ -34,11 +35,24 @@ class StringConvert_Intermid {
         return result;
     }
 
+    private void generate_lexi_internal(int size,int min_size,int max_size,ArrayList<String> all_strings,String current_gen){
+
+        if(all_strings.size() > size) return;
+        if (current_gen.length() > max_size) return;
+        if (current_gen.length() >= min_size){
+            all_strings.add(current_gen);
+        }
+        for(int i=(int)'a' ;i<=(int)'z';i++){
+            generate_lexi_internal(size,min_size,max_size,all_strings,current_gen+ (char)i );
+        }
+
+    }
+
     public String[] generate_lexi(int size, int min_size, int max_size){
-        String all_strings[] = new String[size];
+        ArrayList<String> all_strings = new ArrayList<>();
+        generate_lexi_internal(size,min_size,max_size,all_strings,"");
 
-
-        return all_strings;
+        return (String[])all_strings.toArray();
     }
 
     public static void main(String args[]){
